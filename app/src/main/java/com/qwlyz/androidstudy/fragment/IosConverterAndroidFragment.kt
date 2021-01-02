1bf6f5f7bd12cb79a7d94e36d4c0df7d519d1a52
lyzqw <yuwq327037419@163.com>
2021-01-02 09:50:57 +0800

    bugfix: 文件覆盖
    Signed-off-by: lyzqw <yuwq327037419@163.com>

--- app/src/main/java/com/qwlyz/androidstudy/fragment/IosConverterAndroidFragment.kt
+++ app/src/main/java/com/qwlyz/androidstudy/fragment/IosConverterAndroidFragment.kt
@@ -4,7 +4,6 @@ import android.os.Environment
 import android.util.Log
 import com.blankj.utilcode.util.FileIOUtils
 import com.google.gson.Gson
-import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
 import com.qwlyz.androidstudy.BaseFragment
 import com.qwlyz.androidstudy.R
@@ -22,6 +21,7 @@ import java.lang.reflect.Type
 class IosConverterAndroidFragment : BaseFragment() {
 
     val gson = Gson()
+    val disableList = ArrayList<String>()
 
     companion object {
         const val TAG = "wqq"
@@ -36,7 +36,8 @@ class IosConverterAndroidFragment : BaseFragment() {
             val iosArMap = readJson("ios_localizable-ar.json")
             val androidCnMap = readJson("andriod_cn.json")
             val androidArMap = readJson("android_ar.json")
-
+            Log.d(TAG, "Android 阿拉伯数量: " + androidArMap.size)
+            Log.d(TAG, "ios 阿拉伯数量: " + iosArMap.size)
             assertMapValue(iosCnMap, androidCnMap)
 
             var iosCn: String = ""
@@ -57,23 +58,21 @@ class IosConverterAndroidFragment : BaseFragment() {
                     Log.e(TAG, "找不到的内容" + iosCn)
                 }
             }
-
-            val json = JSONObject(androidArMap).toString()
-
-            val sdDir = Environment.getExternalStorageDirectory()
-            val sdpath: String = sdDir.absolutePath
-            val filepath = sdpath + File.separator.toString() + "android_ar.json"
-            val result = FileIOUtils.writeFileFromString(filepath, json)
-            Log.d(TAG, "initData: 写入成功 result $result, 文件: ${File(filepath).exists()} , 路径 ${filepath}")
+            writeJsonToSDcard(JSONObject(androidArMap).toString(),"android_ar.json")
+            writeJsonToSDcard(Gson().toJson(disableList).toString(),"不满足的文案.json")
         }
     }
 
-    private fun writeJson(androidArMap: MutableMap<String, String>): String {
-        val gson = Gson()
-        val jsonTree = gson.toJsonTree(androidArMap, MutableMap::class.java)
-        val jsonObject = JsonObject()
-        jsonObject.add("General", jsonTree)
-        return jsonObject.toString()
+    private fun writeJsonToSDcard(json: String,fileName:String) {
+
+        val sdDir = Environment.getExternalStorageDirectory()
+        val sdpath: String = sdDir.absolutePath
+        val filepath = sdpath + File.separator.toString() + fileName
+        val result = FileIOUtils.writeFileFromString(filepath, json)
+        Log.d(
+            TAG,
+            "initData: 写入成功 result $result, 文件: ${File(filepath).exists()} , 路径 ${filepath}"
+        )
     }
 
 
@@ -90,6 +89,7 @@ class IosConverterAndroidFragment : BaseFragment() {
     }
 
     var awaitCount = 0
+    var changeCount = 0
 
     private fun changeSingleText(
         androidCnMap: MutableMap<String, String>,
@@ -97,11 +97,17 @@ class IosConverterAndroidFragment : BaseFragment() {
         androidArMap: MutableMap<String, String>,
         iosAr: String
     ) {
+
+        val ios = iosCn.replace(" ","")
         for ((adCnKey, adCnValue) in androidCnMap) {
-            if (iosCn == adCnValue && androidArMap[adCnKey] !=iosAr) {
+            val android = adCnValue.replace(" ","")
+            if (ios == android) {
                 val awaitValue = androidArMap[adCnKey]
                 awaitCount++
-                Log.d(TAG, "可以修改的数据: $awaitCount 带修改的文案: awaitValue: $awaitValue => 他的中文: $adCnKey 正确的文案: $iosAr")
+                Log.d(
+                    TAG,
+                    "可以修改的数据: $awaitCount 待修改的文案: awaitValue: $awaitValue => 他的中文: $adCnKey 正确的文案: $iosAr"
+                )
                 androidArMap[adCnKey] = iosAr
                 return
             }
@@ -115,6 +121,7 @@ class IosConverterAndroidFragment : BaseFragment() {
             dis_count++
             Log.e(TAG, "这个文案不包含: " + iosCn)
             Log.e(TAG, "不满足数量: " + dis_count)
+            disableList.add(iosCn)
         } else {
             ok_count++
 //            Log.d(TAG, "assertValue: 满足数量" + ok_count + "; 总数: " + androidCnMap.size)
